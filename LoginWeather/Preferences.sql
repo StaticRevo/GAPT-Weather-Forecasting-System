f406715704d8a13142856550a52a6d59f3bb380d
Isaac Attard <116385849+StaticRevo@users.noreply.github.com>
2024-03-10 16:30:58 +0100

    Add files via upload

---
+++ LoginWeather/Preferences.sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Mar 10, 2024 at 04:30 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `WeatherDatabase`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Preferences`
+--
+
+CREATE TABLE `Preferences` (
+  `user_id` int(11) NOT NULL,
+  `temperature_unit` text NOT NULL DEFAULT '\'\\\'Celcuis\\\'\'',
+  `distance_unit` text NOT NULL DEFAULT '\'Meters\'',
+  `precipitation_unit` text DEFAULT '\'Millimeters\'',
+  `wind_unit` text NOT NULL DEFAULT '\'kmh\''
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `Preferences`
+--
+ALTER TABLE `Preferences`
+  ADD PRIMARY KEY (`user_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
